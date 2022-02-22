52af4e22738ec0430320bf9ee5abba8a2780b27e
6ECCMEPTHIoIIJ <shamraev.alexandr@gmail.com>
2022-02-22 22:56:09 +0300

    task 2

--- sources/main.c
+++ sources/main.c
@@ -1,7 +1,8 @@
 #include "../include/matrix.h"
 #include <assert.h>
 
-#define TASK_1
+//	#define TASK_1
+#define TASK_2
 
 //	Тестирование основных функций библиотеки matrix.h
 #ifdef TEST_MATRIX
@@ -94,3 +95,94 @@ int main() {
 }
 
 #endif // TASK_1
+
+#ifdef TASK_2
+
+/**
+ * @brief Поиск максимального значения элементов массива
+ *
+ * @param arr		указатель на нулевой элемент массива
+ * @param size	кол-во элементов массива
+ * @return	максимальное значение элементов массива
+ */
+int GetMax(int* arr,
+					 const size_t size) {
+	int max = arr[0];
+	for (size_t i = 0; i < size; i++) {
+		if (arr[i] > max) {
+			max = arr[i];
+		}
+	}
+
+	return max;
+}
+
+void sortRowsByMaxElement(Matrix m) {
+	InsertionSortRowsMatrixByRowCriteria(m, GetMax);
+}
+
+static void test_sortRowsByMaxElement_Unsorted() {
+	printf("[--------] Unsorted\n");
+	const size_t kInitialNRows = 3;
+	const size_t kInitialNCols = 3;
+	int initial_arr[] = {1, -7, 9,
+											 0, 128, 11,
+											 -123, -1, -4};
+	int expected_arr[] = {-123, -1, -4,
+												1, -7, 9,
+												0, 128, 11};
+	Matrix m = CreateMatrixFromArray(initial_arr, kInitialNRows,
+																	 kInitialNCols);
+	Matrix expected_m = CreateMatrixFromArray(expected_arr, kInitialNCols,
+																						kInitialNRows);
+	printf("[--------] n_rows = %zu, n_cols = %zu\n",
+				 kInitialNRows,
+				 kInitialNCols);
+	printf("[RUN     ]\n");
+	sortRowsByMaxElement(m);
+	assert(AreTwoMatricesEqual(m, expected_m));
+
+	FreeMemMatrix(&m);
+	FreeMemMatrix(&expected_m);
+	printf("[      OK]\n");
+}
+
+static void test_sortRowsByMaxElement_Sorted() {
+	printf("[--------] Sorted\n");
+	const size_t kInitialNRows = 3;
+	const size_t kInitialNCols = 3;
+	int initial_arr[] = {1, 2, 3,
+											 4, 5, 6,
+											 7, 8, 9};
+	int expected_arr[] = {1, 2, 3,
+												4, 5, 6,
+												7, 8, 9};
+	Matrix m = CreateMatrixFromArray(initial_arr, kInitialNRows,
+																	 kInitialNCols);
+	Matrix expected_m = CreateMatrixFromArray(expected_arr, kInitialNCols,
+																						kInitialNRows);
+	printf("[--------] n_rows = %zu, n_cols = %zu\n",
+				 kInitialNRows,
+				 kInitialNCols);
+	printf("[RUN     ]\n");
+	sortRowsByMaxElement(m);
+	assert(AreTwoMatricesEqual(m, expected_m));
+
+	FreeMemMatrix(&m);
+	FreeMemMatrix(&expected_m);
+	printf("[      OK]\n");
+}
+
+static void test_sortRowsByMaxElement() {
+	printf("[========] %s()\n", __FUNCTION__);
+	test_sortRowsByMaxElement_Unsorted();
+	test_sortRowsByMaxElement_Sorted();
+}
+
+int main() {
+	test_sortRowsByMaxElement();
+
+	return 0;
+}
+
+#endif // TASK_2
