e07e5e1011aff05f379260559fc68515fb287d2a
6ECCMEPTHIoIIJ <shamraev.alexandr@gmail.com>
2022-02-22 22:42:50 +0300

    task 1

--- sources/main.c
+++ sources/main.c
@@ -1,4 +1,11 @@
 #include "../include/matrix.h"
+#include <assert.h>
+
+#define TASK_1
+
+//	Тестирование основных функций библиотеки matrix.h
+#ifdef TEST_MATRIX
+
 #include "../include/matrix_tests.h"
 
 int main() {
@@ -6,3 +13,84 @@ int main() {
 
 	return 0;
 }
+
+#endif // TEST_MATRIX
+
+//	Дана квадратная матрица, все элементы которой различны. Поменять местами
+//	строки, в которых находятся максимальный и минимальный элементы.
+#ifdef TASK_1
+
+/**
+ * @brief Обмен строк матрицы, содержащих минимальный и максимальный элементы
+ *
+ * @param m матрица
+ */
+void SwapRowsWithMinMaxEls(Matrix m) {
+	SwapRows(m, GetMinValuePos(m).row_i, GetMaxValuePos(m).row_i);
+}
+
+static void test_transposeMatrix_DiffRows() {
+	printf("[--------] DiffRows\n");
+	const size_t kInitialNRows = 3;
+	const size_t kInitialNCols = 3;
+	int initial_arr[] = {1, 2, 3,
+											 4, 5, 6,
+											 7, 8, 9};
+	int expected_arr[] = {7, 8, 9,
+												4, 5, 6,
+												1, 2, 3};
+	Matrix m = CreateMatrixFromArray(initial_arr, kInitialNRows,
+																	 kInitialNCols);
+	Matrix expected_m = CreateMatrixFromArray(expected_arr, kInitialNCols,
+																						kInitialNRows);
+	printf("[--------] n_rows = %zu, n_cols = %zu\n",
+				 kInitialNRows,
+				 kInitialNCols);
+	printf("[RUN     ]\n");
+	SwapRowsWithMinMaxEls(m);
+	assert(AreTwoMatricesEqual(m, expected_m));
+
+	FreeMemMatrix(&m);
+	FreeMemMatrix(&expected_m);
+	printf("[      OK]\n");
+}
+
+static void test_transposeMatrix_OneRow() {
+	printf("[--------] OneRow\n");
+	const size_t kInitialNRows = 3;
+	const size_t kInitialNCols = 3;
+	int initial_arr[] = {1, 2, 3,
+											 4, 5, 6,
+											 -6, 8, 9};
+	int expected_arr[] = {1, 2, 3,
+												4, 5, 6,
+												-6, 8, 9};
+	Matrix m = CreateMatrixFromArray(initial_arr, kInitialNRows,
+																	 kInitialNCols);
+	Matrix expected_m = CreateMatrixFromArray(expected_arr, kInitialNCols,
+																						kInitialNRows);
+	printf("[--------] n_rows = %zu, n_cols = %zu\n",
+				 kInitialNRows,
+				 kInitialNCols);
+	printf("[RUN     ]\n");
+	SwapRowsWithMinMaxEls(m);
+	assert(AreTwoMatricesEqual(m, expected_m));
+
+	FreeMemMatrix(&m);
+	FreeMemMatrix(&expected_m);
+	printf("[      OK]\n");
+}
+
+static void test_SwapRowsWithMinMaxEls() {
+	printf("[========] %s()\n", __FUNCTION__);
+	test_transposeMatrix_DiffRows();
+	test_transposeMatrix_OneRow();
+}
+
+int main() {
+	test_SwapRowsWithMinMaxEls();
+
+	return 0;
+}
+
+#endif // TASK_1
