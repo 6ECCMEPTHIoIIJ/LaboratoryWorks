cf22dab52e6509fba27f140ff576087f93cb981c
6ECCMEPTHIoIIJ <shamraev.alexandr@gmail.com>
2022-03-03 13:28:41 +0300

    task 2.1 + task 2.2

--- sources/2_2_remove_extra_spaces.c
+++ sources/2_2_remove_extra_spaces.c
@@ -1,9 +1,11 @@
+#include "../include/mystring.h"
 #include "../tests/tests.h"
 
-void removeAdjacentEqualLetters(char* begin) {
+void removeExtraSpaces(char* begin) {
   char* begin_dst = begin;
+  begin = findNonSpace(begin);
   while (*begin != '\0') {
-    if (*begin != *(begin + 1)) {
+    if (*begin != ' ' || (*(begin + 1) != ' ' && *(begin + 1) != '\0')) {
       *begin_dst = *begin;
       begin_dst++;
     }
@@ -12,50 +14,50 @@ void removeAdjacentEqualLetters(char* begin) {
   *begin_dst = '\0';
 }
 
-void test_removeAdjacentEqualLetters_empty() {
+void test_removeExtraSpaces_empty() {
   char str[] = "";
-  removeAdjacentEqualLetters(str);
+  removeExtraSpaces(str);
   char expected[] = "";
   ASSERT_STRING(expected, str);
   fprintf(stderr, "-------------------\n");
 }
 
-void test_removeAdjacentEqualLetters_allAreEqual() {
+void test_removeExtraSpaces_allAreSpaces() {
   char str[] = "             ";
-  removeAdjacentEqualLetters(str);
-  char expected[] = " ";
+  removeExtraSpaces(str);
+  char expected[] = "";
   ASSERT_STRING(expected, str);
   fprintf(stderr, "-------------------\n");
 }
 
 void test_removeExtraSpaces_notAnySpaces() {
   char str[] = "aabbccdd";
-  removeAdjacentEqualLetters(str);
-  char expected[] = "abcd";
+  removeExtraSpaces(str);
+  char expected[] = "aabbccdd";
   ASSERT_STRING(expected, str);
   fprintf(stderr, "-------------------\n");
 }
 
 void test_removeExtraSpaces_someSpacesAreExtra() {
-  char str[] = "abcdabcd";
-  removeAdjacentEqualLetters(str);
-  char expected[] = "abcdabcd";
+  char str[] = "asd s    ff a";
+  removeExtraSpaces(str);
+  char expected[] = "asd s ff a";
   ASSERT_STRING(expected, str);
   fprintf(stderr, "-------------------\n");
 }
 
 void test_removeExtraSpaces_extraSpacesOnBounds() {
-  char str[] = "aaabccc d d eee";
-  removeAdjacentEqualLetters(str);
-  char expected[] = "abc d d e";
+  char str[] = "    asd   asd     ";
+  removeExtraSpaces(str);
+  char expected[] = "asd asd";
   ASSERT_STRING(expected, str);
   fprintf(stderr, "-------------------\n");
 }
 
-void test_removeAdjacentEqualLetters() {
+void test_removeExtraSpaces() {
   fprintf(stderr, "-------------------\n");
-  test_removeAdjacentEqualLetters_empty();
-  test_removeAdjacentEqualLetters_allAreEqual();
+  test_removeExtraSpaces_empty();
+  test_removeExtraSpaces_allAreSpaces();
   test_removeExtraSpaces_notAnySpaces();
   test_removeExtraSpaces_someSpacesAreExtra();
   test_removeExtraSpaces_extraSpacesOnBounds();
@@ -64,6 +66,6 @@ void test_removeAdjacentEqualLetters() {
 
 
 int main() {
-  test_removeAdjacentEqualLetters();
+  test_removeExtraSpaces();
   return 0;
 }
